a6ed818d7d22b91934054ae5c4e385dc25a670f0
Lorenzo <lori.vannieuwkerk@gmail.com>
2018-10-10 00:21:19 +0200

    aanpassingen

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hallo Quincy");
+        primaryStage.setTitle("VAT Application");
         primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
