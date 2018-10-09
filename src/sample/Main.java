a8451ac5df4f4926220f3fa7d068f99183e4e699
Lorenzo <lori.vannieuwkerk@gmail.com>
2018-10-09 16:51:30 +0200

    aanpassingen

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,45 +1,24 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
-public class Main extends Application implements EventHandler<ActionEvent> {
-
-    Button button;
+public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Vorm Analyse Tool Application");
-        button = new Button();
-        button.setText("Click me");
-
-        button.setOnAction(this);
-
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
-
-        primaryStage.setScene(new Scene(root, 300, 300));
+        primaryStage.setTitle("Hallo Quincy");
+        primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
-    }
-
-    @Override
-    public void handle(ActionEvent event) {
-        if (event.getSource()==button){
-            System.out.println("Well done, my friend!");
-        }
     }
 }
 
